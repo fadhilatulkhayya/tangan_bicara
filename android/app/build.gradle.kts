a330690c4c4e740cb63e149f9e7be6dfb7dcc0e4
ANJAS Teguh <hyuzuru007@gmail.com>
2025-06-09 11:57:34 +0700

    ndk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.tangan_bicara"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.1.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
